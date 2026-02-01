2200c0aab2f78d1b919a09705feebc25654e6715
andreicosmin02 <andrei.achim.6669@gmail.com>
2026-02-01 21:34:35 +0200

    Added icon

--- app/src/main/java/com/example/retailstoreinventory/ui/screens/OrdersScreen.kt
+++ app/src/main/java/com/example/retailstoreinventory/ui/screens/OrdersScreen.kt
@@ -2,7 +2,6 @@ package com.example.retailstoreinventory.ui.screens
 
 import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
